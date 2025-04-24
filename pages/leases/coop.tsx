f291347976acc6f4bcc7fb74f2910e25cb019951
Nadavlistingsync <Benedeknadav@gmail.com>
2025-04-24 14:30:45 -0400

    fix: recreate pages directory with correct case and all necessary files

---
+++ pages/leases/coop.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import LeaseForm from '../../components/LeaseForm';
+
+const CoopLeasePage = () => {
+  return (
+    <Layout>
+      <div className="max-w-3xl mx-auto py-10 px-4">
+        <h1 className="text-3xl font-bold mb-6">Generate a Coop Lease</h1>
+        <p className="text-gray-600 mb-4">
+          Fill out the lease information below. You'll be asked to pay before the lease is generated.
+        </p>
+        <LeaseForm />
+        {/* Stripe payment and submission logic will go here */}
+      </div>
+    </Layout>
+  );
+};
+
+export default CoopLeasePage; 
