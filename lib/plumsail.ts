cd031574e7d1d72fbfd1d72a4d50745303814e11
Nadavlistingsync <Benedeknadav@gmail.com>
2025-04-28 07:42:00 -0400

    Add townhouse lease integration with Plumsail API

--- lib/plumsail.ts
+++ lib/plumsail.ts
@@ -1,5 +1,25 @@
 export async function sendToPlumsail(data: Record<string, any>) {
-  const response = await fetch('https://us-api.plumsail.com/api/v2/processes/ekqgxhkp/pxqngyj/start', {
+  const leaseType = data.leaseType;
+  let endpoint = '';
+
+  switch (leaseType) {
+    case 'condo':
+      endpoint = 'https://us-api.plumsail.com/api/v2/processes/ekqgxhkp/pxqngyj/start';
+      break;
+    case 'townhouse':
+      endpoint = 'https://us-api.plumsail.com/api/v2/processes/ekqgxhkp/awaconr/start';
+      break;
+    case 'coop':
+      endpoint = 'https://us-api.plumsail.com/api/v2/processes/ekqgxhkp/pxqngyj/start';
+      break;
+    case 'rental':
+      endpoint = 'https://us-api.plumsail.com/api/v2/processes/ekqgxhkp/pxqngyj/start';
+      break;
+    default:
+      throw new Error('Invalid lease type');
+  }
+
+  const response = await fetch(endpoint, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
